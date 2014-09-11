1c93a6f1c72f77a378a475793213726bd5dcea6c
Arturas Lebedevas <metarouter@gmail.com>
2014-09-11 20:12:32 +0100

    basic sequenceFrom implementation; inputSync

--- Scrollable.js
+++ Scrollable.js
@@ -1,12 +1,28 @@
 var View = require('famous/core/View');
+var Scroller = require('famous/views/Scroller');
+var GenericSync = require('famous/inputs/GenericSync');
+var Utility = require('famous/utilities/Utility');
 
 function Scrollable () {
   View.apply(this, arguments);
+
+  this.inputSync = new GenericSync();
+  this._scroller = new Scroller({
+    direction: this.options.direction
+  });
+  this.add(this._scroller);
 }
 
 Scrollable.prototype = Object.create(View.prototype);
 Scrollable.prototype.constructor = Scrollable;
 
-Scrollable.DEFAULT_OPTIONS = {};
+Scrollable.DEFAULT_OPTIONS = {
+  direction: Utility.Direction.Y
+};
 
 module.exports = Scrollable;
+
+Scrollable.prototype.sequenceFrom = function () {
+  this._scroller.sequenceFrom.apply(this._scroller, arguments);
+  this._eventInput.pipe(this.inputSync);
+};
